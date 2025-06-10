3a8ad264936ff891180afad354183f5c1f4577fb
Induce <aa5619721@gmail.com>
2025-06-11 02:09:51 +0300

    main

--- app/src/main/java/com/example/shmr/ui/theme/Theme.kt
+++ app/src/main/java/com/example/shmr/ui/theme/Theme.kt
@@ -1,6 +1,4 @@
 package com.example.shmr.ui.theme
-
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
