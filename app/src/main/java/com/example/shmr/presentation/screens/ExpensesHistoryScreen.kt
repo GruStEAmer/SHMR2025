5a8bef5cc15c42ec12630731acd6038569ac6889
Induce <aa5619721@gmail.com>
2025-06-21 02:05:44 +0300

    add: 1.History for Income/Expenses 2.CustomDatePicker 3.StartEndSumDetail
    update: navigation

---
+++ app/src/main/java/com/example/shmr/presentation/screens/ExpensesHistoryScreen.kt
@@ -0,0 +1,130 @@
+package com.example.shmr.presentation.screens
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.shmr.StartAccount
+import com.example.shmr.domain.model.transaction.TransactionResponse
+import com.example.shmr.presentation.components.CustomDatePicker
+import com.example.shmr.presentation.components.ErrorScreen
+import com.example.shmr.presentation.components.LoadingScreen
+import com.example.shmr.presentation.components.StartEndSumDetail
+import com.example.shmr.presentation.listItems.TransactionListItem
+import com.example.shmr.presentation.state.UiState
+import com.example.shmr.presentation.viewModels.ExpensesViewModel
+import java.time.Instant
+import java.time.LocalDate
+import java.time.ZoneId
+
+
+@Composable
+fun ExpensesHistoryScreen() {
+    val expensesViewModel: ExpensesViewModel = viewModel(factory = ExpensesViewModel.Factory)
+    val uiState = expensesViewModel.expensesUiState
+    val sumTransaction = expensesViewModel.sumExpenses
+
+    var startDate by remember { mutableStateOf(LocalDate.now().withDayOfMonth(1)) }
+    var endDate by remember { mutableStateOf(LocalDate.now()) }
+    var showStartDatePicker by remember { mutableStateOf(false) }
+    var showEndDatePicker by remember { mutableStateOf(false) }
+
+    LaunchedEffect(startDate, endDate) {
+        expensesViewModel.getTransactions(startDate, endDate)
+    }
+
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ){
+        StartEndSumDetail(
+            startDate = startDate,
+            endDate = endDate,
+            changeStartDatePicker = { showStartDatePicker = true },
+            changeEndDatePicker = { showEndDatePicker = true},
+            balance = sumTransaction.toString()
+        )
+        when(uiState){
+            is UiState.Loading -> LoadingScreen()
+            is UiState.Success -> ExpensesHistoryScreenUi(
+                uiState.data,
+            )
+            is UiState.Error -> ErrorScreen(
+                message = uiState.error.message!!,
+                reloadData = {
+                    expensesViewModel.getTransactions(
+                        startDate,
+                        endDate
+                    )
+                }
+            )
+        }
+    }
+    if (showStartDatePicker) {
+        CustomDatePicker(
+            onDateSelected = { timestamp ->
+                timestamp?.let {
+                    startDate = Instant.ofEpochMilli(it)
+                        .atZone(ZoneId.systemDefault())
+                        .toLocalDate()
+                }
+                showStartDatePicker = false
+            },
+            onDismiss = { showStartDatePicker = false }
+        )
+    }
+
+    if (showEndDatePicker) {
+        CustomDatePicker(
+            onDateSelected = { timestamp ->
+                timestamp?.let {
+                    endDate = Instant.ofEpochMilli(it)
+                        .atZone(ZoneId.systemDefault())
+                        .toLocalDate()
+                }
+                showEndDatePicker = false
+            },
+            onDismiss = { showEndDatePicker = false }
+        )
+    }
+
+}
+
+@Composable
+fun ExpensesHistoryScreenUi(
+    transactions: List<TransactionResponse>
+) {
+    LazyColumn(
+        modifier = Modifier.fillMaxSize()
+    ){
+        items(
+            items = transactions,
+            key = { it -> it.id }
+        ) {
+            TransactionListItem(
+                categoryId = it.category.id,
+                categoryName = it.category.name,
+                emoji = it.category.emoji,
+                amount = it.amount,
+                currency = StartAccount.CURRENCY,
+                comment = it.comment,
+                date = it.createdAt
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun ExpensesHistoryScreenPreview() {
+    ExpensesHistoryScreen()
+}
